714dd30eb9d96824f52d967aa64d45385b6f1ab8
Ryota Ikezawa <pavegy@gmail.com>
2025-06-04 03:26:40 +0900

    :green_heart: Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'static',
+	adapter: cloudflare(),
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
